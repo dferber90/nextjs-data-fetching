05829ee890f03c020db84416df27048024efd7ee
Dominik Ferber <dominik.ferber@gmail.com>
2020-05-17 01:04:27 +0200

    primitive approach

--- pages/api/joke.js
+++ pages/api/joke.js
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { random } from "@mikemcbride/dad-jokes";
 
 export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
-}
+  res.status(200).json({ text: random() });
+};
--- pages/index.js
+++ pages/index.js
@@ -1,72 +1,58 @@
-import Head from 'next/head'
+import * as React from "react";
+import Head from "next/head";
 
-export default function Home() {
+const url = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : `http://localhost:3000`;
+
+export async function getServerSideProps() {
+  const res = await fetch(`${url}/api/joke`);
+  const joke = await res.json();
+
+  return { props: { initialJoke: joke.text } };
+}
+
+export default function Home(props) {
+  const [joke, setJoke] = React.useState(props.initialJoke);
+
+  async function refresh() {
+    const res = await fetch(`${url}/api/joke`);
+    const body = await res.json();
+    setJoke(body.text);
+  }
+
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Next.js data fetching</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className="title">Next.js data fetching</h1>
 
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          This example shows how to efficiently fetch data in Next.js
         </p>
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/zeit/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className="card">
+          <h3>Dad says..</h3>
+          <p>{joke}</p>
+          <button type="button" onClick={() => refresh()}>
+            Tell me another one
+          </button>
         </div>
       </main>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
-
       <style jsx>{`
         .container {
-          min-height: 100vh;
+          margin: 2rem auto 0;
           padding: 0 0.5rem;
           display: flex;
           flex-direction: column;
           justify-content: center;
           align-items: center;
+          max-width: 45rem;
         }
 
         main {
@@ -76,41 +62,7 @@ export default function Home() {
           flex-direction: column;
           justify-content: center;
           align-items: center;
-        }
-
-        footer {
           width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
         }
 
         .title {
@@ -129,28 +81,9 @@ export default function Home() {
           font-size: 1.5rem;
         }
 
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
         .card {
+          width: 100%;
           margin: 1rem;
-          flex-basis: 45%;
           padding: 1.5rem;
           text-align: left;
           color: inherit;
@@ -160,33 +93,24 @@ export default function Home() {
           transition: color 0.15s ease, border-color 0.15s ease;
         }
 
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
         .card p {
           margin: 0;
           font-size: 1.25rem;
           line-height: 1.5;
         }
 
-        .logo {
-          height: 1em;
+        .card button {
+          margin: 1rem 0 0 0;
+          font-size: 1rem;
+          border-radius: 10px;
+          background-color: black;
+          padding: 1rem;
+          cursor: pointer;
+          color: white;
         }
 
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
+        .card button:hover {
+          opacity: 0.8;
         }
       `}</style>
 
@@ -205,5 +129,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
 }
